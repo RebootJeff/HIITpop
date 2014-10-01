2e5e8a3ef82240bf42ca1ae36718dada5ed5deee
Jeff Lee <leejeffc@gmail.com>
2014-10-01 01:10:05 -0700

    Refactor comment for maximum precision

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ var env = process.env.NODE_ENV || 'development';
 app.use(express.static(__dirname + '/../dist'));
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev')); // logging
+app.use(morgan('dev')); // log request/response info to console
 
 
 // ============================================================================
