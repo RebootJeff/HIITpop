3b3246acd014d2ccdec50f8d6c6064ab8322df79
Jeff Lee <leejeffc@gmail.com>
2014-10-25 02:10:07 -0700

    Fix typo

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.route('/api/dev')
 // Default to home page
 // TODO: Fix this to serve 404 page as appropriate
 app.get('/*', function(req, res, next) {
-  res.sendfile('index.html', {
+  res.sendFile('index.html', {
     root: __dirname + '/../dist'
   });
 });
